0fbe6b554e8f3d8fe900cc296692acac2e145dd7
teetop <temitope12.taiwo@gmail.com>
2024-08-18 03:00:07 +0100

    Update MyERC20.ts

--- ignition/modules/MyERC20.ts
+++ ignition/modules/MyERC20.ts
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 
-const MyERC20 = buildModule("AvalModule", (m) => {
+const MyERC20 = buildModule("MyERC20", (m) => {
 
 
   const myERC20 = m.contract("MyERC20", ["MyToken", "MTK"]);
